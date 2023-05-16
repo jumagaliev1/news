1a82de24669483b9a4da735f33363989c7d3af21
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-05-16 17:17:39 +0600

    Schedule task for statistics

---
+++ src/main/java/com/github/jumagaliev1/backendAssignment/config/TaskExecutorConfig.java
@@ -0,0 +1,23 @@
+package com.github.jumagaliev1.backendAssignment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class TaskExecutorConfig {
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(25);
+        executor.setThreadNamePrefix("StatisticalTaskExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
---
+++ src/main/java/com/github/jumagaliev1/backendAssignment/model/entity/CountNews.java
@@ -0,0 +1,11 @@
+package com.github.jumagaliev1.backendAssignment.model.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CountNews {
+    private String newsSource;
+    private Long newsCount;
+}
